f2cdc61e33f53bff2b5518244a9a48c96f186700
1mattmcguire <1mattmcguire@example.com>
2025-12-31 17:16:41 +0545

    Add Render domain to ALLOWED_HOSTS

--- SingingBallAndGongHouse/settings.py
+++ SingingBallAndGongHouse/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure--7!+4a9zf3v%as!r88$a612f!vk-*1p11!^0#5ljvzio)$5r#t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['singingbowlandgonghouse.onrender.com', '127.0.0.1', 'localhost']
+
 
 
 # Application definition
